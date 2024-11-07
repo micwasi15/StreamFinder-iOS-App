b17fc32893164ba0256f65b5037ed75dc13caab3
Michal <272674@student.pwr.edu.pl>
2024-11-07 19:18:14 +0100

    Initial Commit

---
+++ StreamFinder/StreamFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamFinderApp.swift
+//  StreamFinder
+//
+//  Created by stud on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct StreamFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
