060db19b8ec5bd309f836a6d89f4882b9d450390
Michal <272674@student.pwr.edu.pl>
2024-11-07 20:16:52 +0100

    start

---
+++ StreamFinder/ProfileView.swift
@@ -0,0 +1,11 @@
+struct ProfileView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Ekran profilu")
+                // Reszta Twojego widoku profilu
+            }
+            .navigationTitle("Profil")
+        }
+    }
+}
