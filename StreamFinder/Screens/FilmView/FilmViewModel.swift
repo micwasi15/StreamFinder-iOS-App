bc2df0f820719f5ae13482267eb63b9f090be577
Michał Wąsiński <micwasi2003@gmail.com>
2024-12-30 22:53:03 +0100

    FilmView changes

--- StreamFinder/Screens/FilmView/FilmViewModel.swift
+++ StreamFinder/Screens/FilmView/FilmViewModel.swift
@@ -1,9 +1,25 @@
 import SwiftUI
 
 class FilmViewModel: ObservableObject {
-    @Published var film: Film = MockData.films[0]
-    
-    func fetchFilm() {
-        
+    @Published var film: Film? 
+    @Published var isLoading: Bool = false  
+    @Published var errorMessage: String?
+
+    func fetchFilm(id: Int) {
+        Task {
+            do {
+                isLoading = true
+                let fetchedFilm = try await APIShowHandler.getFilm(id: id)
+                DispatchQueue.main.async {
+                    self.film = fetchedFilm
+                    self.isLoading = false
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    self.errorMessage = "Failed to fetch film: \(error.localizedDescription)"
+                    self.isLoading = false
+                }
+            }
+        }
     }
 }
