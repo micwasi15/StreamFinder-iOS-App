1350cfe96c210f3e727943464c707602a18e4fd2
Michał Wąsiński <micwasi2003@gmail.com>
2025-01-02 02:09:53 +0100

    Refactored Constants

---
+++ StreamFinder/Utilities/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct Constants {
+    static let bgColor = Color(red: 0.12, green: 0.12, blue: 0.12)
+    static let fgColor = Color(red: 0.96, green: 0.94, blue: 0.97)
+    static let selectedItemColor = Color(.white)
+    static let unselectedItemColor = Color(.white.opacity(0.55))
+    static let tabBgColor = Color(red: 0.14, green: 0.14, blue: 0.14)
+}
