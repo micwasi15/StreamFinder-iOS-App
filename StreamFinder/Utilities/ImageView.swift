288de8e4ca525f1364774a8451622ce160cb4738
Michał Wąsiński <micwasi2003@gmail.com>
2025-01-04 17:24:52 +0100

    Changed Image extension

--- StreamFinder/Utilities/ImageView.swift
+++ StreamFinder/Utilities/ImageView.swift
@@ -1,23 +1,11 @@
 import SwiftUI
 
 extension Image {
-    static func fromFolder(named name: String, folder: String) -> Image? {
+    static func fromFolder(named name: String) -> Image? {
         if let path = Bundle.main.path(forResource: "\(name)", ofType: "png"),
            let uiImage = UIImage(contentsOfFile: path) {
             return Image(uiImage: uiImage)
         }
         return nil
-    }
-
-    static func fromLogosFolder(named name: String) -> Image? {
-        return fromFolder(named: name, folder: "logos")
-    }
-
-    static func fromFlagsFolder(named name: String) -> Image? {
-        return fromFolder(named: name, folder: "flags")
-    }
-
-    static func fromIconsFolder(named name: String) -> Image? {
-        return fromFolder(named: name, folder: "icons")
     }
 }
