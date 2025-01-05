34f270d09264a69e3473b9a6d04f1e042a478110
micwasi15 <272674@student.pwr.edu.pl>
2025-01-05 17:27:57 +0100

    Fixed LoadingScreenView

--- StreamFinder/Views/LoadingScreenView.swift
+++ StreamFinder/Views/LoadingScreenView.swift
@@ -13,75 +13,72 @@ struct LoadingScreenView: View {
     var body: some View {
         VStack {
             ZStack {
-                GeometryReader { geometry in
-                    Trapezoid()
-                        .fill(LinearGradient(
+                Trapezoid()
+                    .fill(
+                        LinearGradient(
                             gradient: Gradient(colors: [.yellow, .clear]),
                             startPoint: .trailing,
                             endPoint: .leading
-                        ))
-                        .opacity(trapezoidOpacity)
-                        .frame(width: geometry.size.width * 0.35, height: geometry.size.height * 0.09)
-                        .position(x: -10, y: 430)
-                }
+                        )
+                    )
+                    .opacity(trapezoidOpacity)
+                    .frame(width: 80, height: 50)
+                    .offset(x: -110, y: 36)
                 
                 if let img = Image.fromFolder(named: "projector") {
                     img
                         .resizable()
                         .scaledToFit()
-                        .position(x: 100, y: 385)
+                        .frame(width: 150)
                         .colorMultiply(imageColor)
-                    
                 }
                 
-                HStack {
+                HStack(spacing: 10) {
                     if let img = Image.fromFolder(named: "tape") {
                         img
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 75)
+                            .frame(width: 60)
                             .rotationEffect(.degrees(rotation1))
-                            .position(x: 75, y: 335)
                             .colorMultiply(imageColor)
+                            .offset(x: 15)
                     }
                     
                     if let img = Image.fromFolder(named: "tape") {
                         img
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 75)
+                            .frame(width: 60)
                             .rotationEffect(.degrees(rotation2))
-                            .position(x: 50, y: 335)
                             .colorMultiply(imageColor)
+                            .offset(x: 10)
                     }
                 }
-            }
-            .frame(width: 200)
-            .padding(.bottom, 0)
-            //.background(Constants.bgColor)
-            .onAppear {
-                withAnimation(
-                    Animation.linear(duration: 10).repeatForever(autoreverses: false)
-                ) {
-                    rotation1 += 360
-                    rotation2 += 360
-                }
-                
-                withAnimation(Animation.easeInOut(duration: 4).repeatForever(autoreverses: true)) {
-                    trapezoidOpacity = 0.5
-                }
+                .offset(y: -40)
             }
             
             if let text = text {
                 Text(text)
-                    .lineLimit(1, reservesSpace: true)
-                    .padding(.bottom, 250)
                     .font(.title2)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 20)
             }
         }
-        .frame(maxWidth: .infinity)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Constants.bgColor)
         .foregroundStyle(Constants.fgColor)
+        .onAppear {
+            withAnimation(
+                Animation.linear(duration: 10).repeatForever(autoreverses: false)
+            ) {
+                rotation1 += 360
+                rotation2 += 360
+            }
+            
+            withAnimation(Animation.easeInOut(duration: 3).repeatForever(autoreverses: true)) {
+                trapezoidOpacity = 0.3
+            }
+        }
     }
 }
 
